b7fe41ad41befbc540a68761b77b1b7fe5df0159
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-07-22 11:33:45 +0600

    add row button dynamic made

--- src/app/shared/components/sub-panel-header/sub-panel-header.ts
+++ src/app/shared/components/sub-panel-header/sub-panel-header.ts
@@ -1,4 +1,4 @@
-import {Component, input} from '@angular/core';
+import {Component, input, output} from '@angular/core';
 
 @Component({
   selector: 'app-sub-panel-header',
@@ -8,4 +8,10 @@ import {Component, input} from '@angular/core';
 })
 export class SubPanelHeader {
   subPanelTitle = input<string>();
+  isButton = input<boolean>();
+  addAction = output();
+
+  add() {
+    this.addAction.emit();
+  }
 }
