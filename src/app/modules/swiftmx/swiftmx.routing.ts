aa595628d783bcd5c95f9685728ce23e3876ee2e
mainul <mainul.hasan@leads-bd.com>
2025-08-06 11:15:52 +0600

    pacs002 modified

--- src/app/modules/swiftmx/swiftmx.routing.ts
+++ src/app/modules/swiftmx/swiftmx.routing.ts
@@ -1,7 +1,10 @@
 import {Routes} from '@angular/router';
 import {Pacs008} from './feature/pacs-008/pacs-008';
+import {Pacs009} from './feature/pacs-009/pacs-009';
+import {Pacs002} from './feature/pacs-002/pacs-002';
 
 export const SwiftmxRouting: Routes = [
   {path: 'pacs-008', component: Pacs008},
   {path: 'pacs-009', component: Pacs009},
+  {path: 'pacs-002', component: Pacs002},
 ];
