3c93a4d538abdadf7d37b0d9446d527cc453be2d
mahfuz <mahfuzur.rahman.swe@gmail.com>
2025-08-17 17:19:57 +0600

    login page functional

--- src/app/core/auth/login/login.ts
+++ src/app/core/auth/login/login.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   imports: [
-    RouterLink
+    ReactiveFormsModule
   ],
   templateUrl: './login.html',
   styleUrl: './login.scss'
 })
-export class Login {
+export class Login implements OnInit {
+
+
+  http = inject(HttpClient);
+  fb = inject(FormBuilder);
+  router = inject(Router);
+
+  frmGroup: FormGroup;
+  loginError: boolean = false;
+
+  ngOnInit(): void {
+    this.frmGroup = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    })
+  }
+
+  doLogin() {
+    this.loginError = false;
+    this.http.post('http://localhost:8096/swiftCoreAccess/api/v1/auth/login', this.frmGroup.value).subscribe((response: any) => {
+      if (response.status) {
+        this.router.navigateByUrl('/dashboard');
+      } else this.loginError = true;
+    }, error => {
+      this.loginError = true;
+    })
+  }
 
 }
